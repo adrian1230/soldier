33bef323047e277d6586119294d269b8fab8437f
adrian1230 <ichbingesenet@gmail.com>
2021-06-28 01:12:10 +0800

    adrian

--- fabfile.py
+++ fabfile.py
@@ -12,13 +12,24 @@ def listdirmain(ctx):
 			f.run("ls -la")
 
 @task
-def installbasic(ctx):
+def createnewapp(ctx):
+        with conn(
+		"172.105.4.75",
+		"root",
+		connect_kwargs={"key_filename":"../../.ssh/id_rsa.pub}
+		) as f:
+                	with f.cd("/var/www/html/"):
+				f.run("npx create-react-app myapp")
+				with f.cd("/myapp"):
+					f.run("ls -la")
+
+@task
+def checkstatus(ctx):
 	with conn(
 		"172.105.4.75",
 		user="root",
 		connect_kwargs={"key_filename":"../../.ssh/id_rsa.pub"}
 	) as c:
-		c.run("yum install mariadb")
 		c.run("service httpd restart")
 		c.run("systemctl status httpd.service")
 
